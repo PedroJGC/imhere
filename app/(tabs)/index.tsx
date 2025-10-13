773dbd77670e04f394bc19846348e41398dd566d
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-13 18:45:54 -0300

    fix: add keys to Text components for improved rendering

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,10 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo ao seu app!</Text>
-      <Text>Iniciando no expo!</Text>
+      <Text key="1" style={styles.text}>
+        Bem-vindo ao seu app!
+      </Text>
+      <Text key="2">Iniciando no expo!</Text>
     </View>
   )
 }
